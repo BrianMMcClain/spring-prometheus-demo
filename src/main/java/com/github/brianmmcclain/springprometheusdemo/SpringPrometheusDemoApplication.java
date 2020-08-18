57c449e68c0e60bd07f34285eb37c11ad533b0f5
Brian McClain <brianmmcclain@gmail.com>
2020-08-18 16:15:53 -0400

    Initial commit

---
+++ src/main/java/com/github/brianmmcclain/springprometheusdemo/SpringPrometheusDemoApplication.java
@@ -0,0 +1,13 @@
+package com.github.brianmmcclain.springprometheusdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPrometheusDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPrometheusDemoApplication.class, args);
+	}
+
+}
